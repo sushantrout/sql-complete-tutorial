b985564b5f90d4bb48858b190841780e3104abe5
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2023-07-31 09:27:02 +0530

    initoal

---
+++ create-table.sql
@@ -0,0 +1,11 @@
+-- Step 1: Create a custom domain type for gender
+CREATE DOMAIN gender_enum AS CHAR(1)
+    CHECK (VALUE IN ('M', 'F'));
+
+create table employee(id int PRIMARY KEY, name varchar(200), gender gender_enum NOT NULL DEFAULT 'M');
+
+insert into employee values(1, 'sushan', 'M');
+
+CREATE TYPE gender_enumz AS ENUM ('M', 'F');
+create table employee1(id int PRIMARY KEY, name varchar(200), gender gender_enumz NOT NULL DEFAULT 'M');
+insert into employee1 values(1, 'sushan', 'M');
